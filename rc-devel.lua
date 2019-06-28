d0257f070e109e58ac57aba3f1d2996089647fcc
Aleksej <pokaprivet10@gmail.com>
2019-06-28 09:43:38 +0300

    Edit

--- rc-devel.lua
+++ rc-devel.lua
@@ -17,8 +17,10 @@ lock.autostart = true
 --local rc = "color.orange.rc-orange"
 --local rc = "color.green.rc-green"
 
-local rc = "shade.ruby.rc-ruby"
+--local rc = "shade.ruby.rc-ruby"
 --local rc = "shade.steel.rc-steel"
+
+local rc = "color.lixir_custom.rc-lixir_custom"
 
 require(rc)
 
